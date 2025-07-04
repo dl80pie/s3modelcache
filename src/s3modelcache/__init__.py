5dac288dc7f18c3642314d8e533524eca467fc17
Jens Pietsch <mail@pietschnet>
2025-07-04 15:27:09 +0200

    refactor: rename upload_large_model to upload_large_model_to_hcp with backwards compatibility

--- src/s3modelcache/__init__.py
+++ src/s3modelcache/__init__.py
@@ -9,4 +9,6 @@ Classic src/ layout:
 """
 from .model_cache import S3ModelCache  # noqa: F401
 from .logger import HCPLogger, LoggedHCPCache  # noqa: F401
-from .upload_large import upload_large_model  # noqa: F401
+from .upload_large import upload_large_model_to_hcp  # noqa: F401
+#Backward-compat alias
+upload_large_model = upload_large_model_to_hcp
